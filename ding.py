06fc61edace297db2a2ef31288ac23cc9b33012c
liviu- <hi@liviu.me>
2016-09-30 17:04:57 +0300

    Add basic functionality

---
+++ ding.py
@@ -0,0 +1,46 @@
+import sys
+import time
+import datetime
+
+ERROR_MSG = 'Invalid arguments'
+
+class TimeParser():
+
+    time_map = {
+        's': 1,
+        'm': 60,
+        'h': 60 * 60,
+    }
+
+    def __init__(self, time, relative):
+        self.time = time
+        self.relative = relative
+
+    def get_seconds(self):
+        return self._get_seconds_relative() if self.relative else self._get_seconds_absolute()
+
+    def _get_seconds_relative(self):
+        return sum([self.time_map[t[-1]] *  int(t[:-1]) for t in self.time])
+
+    def _get_seconds_absolute(self):
+        user_time = datetime.datetime.combine(datetime.date.today(), datetime.time(*map(int, self.time[0].split(':'))))
+        now = datetime.datetime.now()
+        return (user_time - now).seconds if user_time > now else sys.exit('The time provided must be in the future')
+
+
+def beep(seconds):
+    time.sleep(seconds)
+    sys.stdout.write('\a')
+    sys.stdout.flush()    
+
+
+def parse_time(args):
+    relative = True if args[0] == 'in' else False if args[0] == 'at' else sys.exit(ERROR_MSG)
+    user_time = args[1:]
+    parser = TimeParser(user_time, relative)
+    seconds = parser.get_seconds()
+    beep(seconds)
+
+
+if __name__ == '__main__':
+    parse_time(sys.argv[1:])
